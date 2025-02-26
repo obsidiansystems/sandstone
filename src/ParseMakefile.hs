1de5c6ebb6a8583bf81f2329def208ced14b0f5f
Luigy Leon <luigy.leon@obsidian.systems>
2025-02-26 15:54:06 -0500

    Parse Makefile and record nodes

--- src/ParseMakefile.hs
+++ src/ParseMakefile.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module ParseMakefile where
 
-import Control.Monad (guard)
+import Control.Monad (guard, mapM_)
+import Data.List.NonEmpty 
 import Data.Maybe (catMaybes)
 import Data.Text (Text)
 import Data.Text qualified as T
@@ -13,13 +15,33 @@ import Graph
 
 main :: IO ()
 main = do
-  makefile <- T.readFile "Makefile"
+  makefile <- T.readFile "example/Makefile"
   print makefile
-  print $ catMaybes $ parseModuleLine <$> T.lines makefile
-  pure ()
+  let modules = catMaybes $ parseModuleLine <$> T.lines makefile
+  mapM_ print modules
 
-parseModuleLine :: Text -> Maybe Text
+parseModuleLine :: Text -> Maybe ((ModuleName, Text), (ModuleName, Text))
 parseModuleLine line = do
   guard $ not $ "#" `T.isPrefixOf` line
-  let (l, r) = T.breakOn " : " line
-  pure r
+  let (target, prereq) = T.breakOn " : " line
+  file <- T.stripPrefix " : " prereq
+  (moduleName, ext) <- parseFilePath file
+  (targetName, targetExt) <- parseFilePath target
+  pure ((targetName, targetExt), (moduleName, ext))
+ where
+  parseFilePath file = do
+    let (path, ext) = T.breakOn "." file
+    moduleName <- nonEmpty $ T.splitOn "/" path
+    pure (moduleName, ext)
+
+recordNode :: ((ModuleName, Text), (ModuleName, Text)) -> Maybe (Node, [Edge])
+recordNode = \case
+  ((moduleName, ".o"), (moduleName', ".hs")) 
+    | moduleName == moduleName' -> Just (Node_Compile moduleName, [])
+  ((moduleName, ".o-boot"), (moduleName', ".hs")) 
+    | moduleName == moduleName' -> Just (Node_PreCompile moduleName, [])
+  ((moduleName, ".o"), (interfaceName, ".hi")) -> 
+    Just (Node_Compile moduleName, [Edge_Interface $ Node_Compile interfaceName])
+  ((moduleName, ".o-boot"), (interfaceName, ".hi")) ->
+    Just (Node_PreCompile moduleName, [Edge_Interface $ Node_Compile interfaceName])
+  _ -> Nothing
