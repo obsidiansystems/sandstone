deea20b03a3920923733fb248c925a21d53f41a3
John Ericson <John.Ericson@Obsidian.Systems>
2025-02-27 19:36:20 -0500

    Remove more warnings

--- src/Sandstone/Error.hs
+++ src/Sandstone/Error.hs
@@ -1,15 +1,10 @@
 module Sandstone.Error where
 
-import Control.Monad
 import Data.Bifunctor
-import Data.Foldable
 import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty qualified as NEL
 import Data.Text (Text)
-import Data.Text qualified as T
 import Data.Validation
-import System.Nix.Derivation
-import System.Nix.StorePath
 
 type ErrorForest = NonEmpty ErrorTree
 
