6cf9dd70fe6806006af9ad7d7093db6c62dbe4dd
John Ericson <John.Ericson@Obsidian.Systems>
2025-02-27 19:48:12 -0500

    Start to use hnix-store

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
+import Data.Dependent.Sum
 import Data.Graph
+import Data.Map qualified as Map
 import Data.Maybe (catMaybes)
+import Data.Set qualified as Set
+import Data.Some
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Data.Validation
+import Data.Vector qualified as V
+import System.Nix.ContentAddress
+import System.Nix.Derivation
+import System.Nix.Hash
+import System.Nix.OutputName
+import System.Nix.StorePath
 
 import Sandstone.Error
 import Sandstone.MakefileParse qualified as MakefileParse
@@ -28,3 +38,25 @@ main = do
   let (graph, _) = graphFromEdges' $ fmap (\(node, edges') -> (node, node, edges')) nodes
   print graph
 
+
+exampleDrv :: Derivation
+exampleDrv = Derivation
+  { name = bad "hello-from-sandstone"
+  , outputs = DerivationType_ContentAddressing :=> let
+      outputSpec = ContentAddressedDerivationOutput
+        { caMethod = ContentAddressMethod_NixArchive
+        , caHashAlgo = Some HashAlgo_SHA256
+        }
+    in Map.fromList $ (\on -> (OutputName $ bad on, outputSpec)) <$> ["object", "interface"]
+  , inputs = DerivationInputs
+    { srcs = Set.empty -- /nix/store/asdfasdfasdf-Foo.hs will go here
+    , drvs = mempty -- /nix/store/asdfasdfasdf-Foo.drv^interface will go here
+    }
+  , platform = "x86_64-linux"
+  , builder = "echo"
+  , args = V.fromList ["-c"]
+  , env = Map.empty
+  }
+
+ where
+   bad = either (error . show) id . mkStorePathName
