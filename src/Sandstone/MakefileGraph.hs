d064afd8c115f7b1871ce60eac32bfe66e49f35b
John Ericson <John.Ericson@Obsidian.Systems>
2025-03-04 17:33:35 -0500

    Delete `Node` entirely

--- src/Sandstone/MakefileGraph.hs
+++ src/Sandstone/MakefileGraph.hs
@@ -8,7 +8,17 @@ import Data.List.NonEmpty (NonEmpty)
 -- | E.g. "Foo.Bar.Baz" becomes '["Foo", "Bar", "Baz"]'
 type ModuleName = NonEmpty Text
 
--- | The key for a module proper, or boot module interface
+-- | The key for a module proper, or boot module:w
+--
+-- See https://downloads.haskell.org/ghc/latest/docs/users_guide/separate_compilation.html
+--
+--   if 'hsBoot'
+--
+-- - then for `hs-boot` file into `.o-boot` (mostly fake) and `.hi-boot` files,
+--
+-- - else for `hs` file into `o` and `hi` files
+--
+-- Dependencies on other modules are always on their hi file outputs
 data Module = Module
   -- | Whether we mean the module proper, or its boot interface
   --
@@ -17,22 +27,6 @@ data Module = Module
   -- | It's name
   , moduleName :: ModuleName
   }
-  deriving (Show, Ord, Eq)
-
--- | See https://downloads.haskell.org/ghc/latest/docs/users_guide/separate_compilation.html
-data Node
-  -- | if 'hsBoot'
-  --
-  -- - then for `hs-boot` file into `.o-boot` (mostly fake) and `.hi-boot` files,
-  --
-  -- - else for `hs` file into `o` and `hi` files
-  --
-  -- Dependencies on other modules are always on their hi file outputs
-  = Node_Compile Module
-  -- | Not sure how to get from Makefile
-  --
-  -- Dependencies on other modules are always on their o file outputs
-  | Node_Link
   deriving (Show, Ord, Eq)
 
 -- | Path without file extension
